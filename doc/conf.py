63304cabd10e82e1c7fe33eebc14d8dfbfee7c0a
Keshav Narasimhan <keshavnarasimhan123@gmail.com>
2021-02-11 01:33:35 -0600

    Python 2 to Python 3

--- doc/conf.py
+++ doc/conf.py
@@ -52,8 +52,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'openbandparams'
-copyright = u'2013-2015, Scott J Maddox'
+project = 'openbandparams'
+copyright = '2013-2015, Scott J Maddox'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -208,8 +208,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'openbandparams.tex', u'openbandparams Documentation',
-   u'Scott J Maddox', 'manual'),
+  ('index', 'openbandparams.tex', 'openbandparams Documentation',
+   'Scott J Maddox', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -238,8 +238,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'openbandparams', u'openbandparams Documentation',
-     [u'Scott J Maddox'], 1)
+    ('index', 'openbandparams', 'openbandparams Documentation',
+     ['Scott J Maddox'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -252,8 +252,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'openbandparams', u'openbandparams Documentation',
-   u'Scott J Maddox', 'openbandparams', 'One line description of project.',
+  ('index', 'openbandparams', 'openbandparams Documentation',
+   'Scott J Maddox', 'openbandparams', 'One line description of project.',
    'Miscellaneous'),
 ]
 
